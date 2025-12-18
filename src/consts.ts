c21a9a2f5cbf8fd6cba72366e4b81c0fe762012f
Phil Giammattei <philgiammattei@gmail.com>
2025-12-18 12:03:01 -0500

    update meta description

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Astrolab.ist";
-export const SITE_DESCRIPTION = "Cool astrology stuff from Jes.";
+export const SITE_DESCRIPTION = "Astrolab.ist is the home of Jes Wolfe!, toolmaker, software heretic, dabbler and friend of the dials.";
 export const BASE_URL = "https://astrolabist.netlify.app";
